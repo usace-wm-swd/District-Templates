e921b7c7813481723db210c51c525520be3f1810
Daniel Osborne <Daniel.T.Osborne@usace.army.mil>
2024-01-17 16:03:54 -0800

    Sync up with changes made to SPK site.

---
+++ SPK/js/gov-banner.js
@@ -0,0 +1,50 @@
+// Controls the dropdown for the official gov header banner
+const SCROLL_AMOUNT_PCT = 0.0
+const GOV_BANNER = document.querySelector("#gov-banner")
+const GOV_BANNER_EXPAND = document.querySelector(".gov-banner-expand")
+function toggleGovBanner(active) {
+    if (active) {
+        document.querySelector(".gov-banner-panel").classList.remove("active")
+        document.querySelector(".gov-banner-expand").classList.remove("active")
+    } else {
+        document.querySelector(".gov-banner-panel").classList.add("active")
+        document.querySelector(".gov-banner-expand").classList.add("active")
+    }
+}
+let banner_scroll_px, scroll_banner_threshold_px;
+function createScrollListener() {
+    // Create listener to hide the gov banner on scroll
+    // Update to the new page size
+    // Get 15% of the page height
+    banner_scroll_px = window.innerHeight * SCROLL_AMOUNT_PCT
+    // Set a minimum threshold of 25 pixels
+    scroll_banner_threshold_px = banner_scroll_px < 25 ? 25 : banner_scroll_px
+    return document.addEventListener("scroll", () => {
+        if (document.body.scrollTop > scroll_banner_threshold_px || document.documentElement.scrollTop > scroll_banner_threshold_px) {
+            toggleGovBanner(true)
+            GOV_BANNER.classList.add("slide-out")
+        } else
+            GOV_BANNER.classList.remove("slide-out")
+    })
+}
+// When the user scrolls, hide the Gov Banner at 10% the screen height
+let govBannerScrollEvent = createScrollListener()
+
+// Let the user click anywhere inside the banner to expand
+GOV_BANNER.addEventListener("click", (e)=> {
+    e.preventDefault()
+    // Remove the listener so the expansion does not get hidden
+    removeEventListener("scroll", govBannerScrollEvent)
+    toggleGovBanner(GOV_BANNER_EXPAND.classList.contains("active"))
+    govBannerScrollEvent = createScrollListener()
+})
+// Enable the enter key on tab + enter of the gov message
+GOV_BANNER_EXPAND.addEventListener("keypress", (e)=> {
+    if (e.key === "Enter")
+        e.target.click()
+})
+
+// Show the banner on page load
+window.addEventListener("load",()=> {
+    GOV_BANNER.classList.remove("hide")
+})
--- SPK/js/header.js
+++ SPK/js/header.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', 
+function () {
   var coll_mob = document.getElementsByClassName("collapsible-mobile");
   for (let col_m_idx = 0; col_m_idx < coll_mob.length; col_m_idx++) {
     coll_mob[col_m_idx].addEventListener("click", function (e) {
@@ -13,22 +14,37 @@ $(document).ready(function () {
       }
     });
   }
-});
+}, false);
 
 // Mobile Burger Bar
 function openNav() {
   var mobileNav = document.getElementById("mobileNav");
   mobileNav.classList.add("open");
   var child = document.getElementById("mobileNavContent");
-  mobileNav.style.right = child.clientWidth - child.offsetWidth + "px";
   var open = document.getElementById("burgerBtn");
   open.style.display = "none";
   var child = document.getElementById("overlay-content");
+}
+
+// Mobile Search
+function toggleSearch() {
+  var mobileNav = document.getElementById("main-search");
+  if(mobileNav.classList.contains("search-open")) {
+    mobileNav.classList.remove("search-open");
+  } else {
+    mobileNav.classList.add("search-open");
+  }
+  var button = document.getElementById("search-form-trigger");
+  if(button.classList.contains("search-open")) {
+    button.classList.remove("search-open");
+  } else {
+    button.classList.add("search-open");
+  }
 }
 
 function closeNav() {
   var myNav = document.getElementById("mobileNav");
   mobileNav.classList.remove("open");
   var open = document.getElementById("burgerBtn");
-  open.style.display = "inline-block";
+  open.style.display = null;
 }
